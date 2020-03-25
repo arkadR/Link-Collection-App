6d3dac8ccd551a95184a814e6cb845bcadf2318d
arkadR <arkadiusz.rasz@gmail.com>
2020-03-25 18:30:09 +0100

    Updated Schema script

--- Main/Program.cs
+++ Main/Program.cs
@@ -3,18 +3,21 @@ using Microsoft.Extensions.Hosting;
 
 namespace LinkCollectionApp
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+      return Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
+    }
+  }
 }
--- Schema.sql
+++ Schema.sql
@@ -1,45 +1,36 @@
-CREATE TABLE [User]
-(
-    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    [FirstName] VARCHAR(50),
-    [LastName] VARCHAR(50),
-    [Email] VARCHAR(50) NOT NULL,
-    [RegisterDate] DATETIME NOT NULL DEFAULT GETDATE(),
-);
-
-CREATE TABLE [Collection]
+﻿CREATE TABLE [app].[Collection]
 (
     [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     [IsPublic] BIT DEFAULT 0,
-    [OwnerId] INT NOT NULL FOREIGN KEY REFERENCES [User]([Id]),
+    [OwnerId] NVARCHAR(450) NOT NULL FOREIGN KEY REFERENCES [dbo].[AspNetUsers]([Id]),
     [Name] VARCHAR(100),
     [Description] VARCHAR(255),
     [CreatedDate] DATETIME NOT NULL DEFAULT GETDATE(),
 );
 
-CREATE TABLE [Element]
+CREATE TABLE [app].[Element]
 (
     [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [Collection]([Id]),
-    [OwnerId] INT NOT NULL FOREIGN KEY REFERENCES [User]([Id]),
+    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [app].[Collection]([Id]),
+    [OwnerId] NVARCHAR(450) NOT NULL FOREIGN KEY REFERENCES [dbo].[AspNetUsers]([Id]),
     [Link] VARCHAR(255) NOT NULL,
     [Name] VARCHAR(100),
     [Description] VARCHAR(255),
-    [Sequence] INT,
+    [Sequence] INT
 );
 
-CREATE TABLE [SharedCollection]
+CREATE TABLE [app].[SharedCollection]
 (
-    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [Collection]([Id]),
-    [UserId] INT NOT NULL FOREIGN KEY REFERENCES [User]([Id]),
+    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [app].[Collection]([Id]),
+    [UserId] NVARCHAR(450) NOT NULL FOREIGN KEY REFERENCES [dbo].[AspNetUsers]([Id]),
     [ViewRights] BIT DEFAULT 1,
     [EditRights] BIT DEFAULT 0,
     PRIMARY KEY ([UserID], [CollectionID])
 );
 
-CREATE TABLE [SavedCollection]
+CREATE TABLE [app].[SavedCollection]
 (
-    [UserId] INT NOT NULL FOREIGN KEY REFERENCES [User]([Id]),
-    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [Collection]([Id]),
+    [UserId] NVARCHAR(450) NOT NULL FOREIGN KEY REFERENCES [dbo].[AspNetUsers]([Id]),
+    [CollectionId] INT NOT NULL FOREIGN KEY REFERENCES [app].[Collection]([Id]),
     PRIMARY KEY ([UserId], [CollectionId])
 );
