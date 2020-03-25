692d357b2db68b5318b06e259e29aeb4737512cf
arkadR <arkadiusz.rasz@gmail.com>
2020-03-25 18:52:58 +0100

    Working models

--- Main/Data/ApplicationUser.cs
+++ Main/Data/ApplicationUser.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using LinkCollectionApp.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace LinkCollectionApp.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+
+      public virtual ICollection<Collection> Collection { get; set; }
+
+      public virtual ICollection<Element> Element { get; set; }
+
+      public virtual ICollection<SavedCollection> SavedCollection { get; set; }
+
+      public virtual ICollection<SharedCollection> SharedCollection { get; set; }
     }
 }
---
+++ Main/Models/SharedCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using LinkCollectionApp.Data;
+
+namespace LinkCollectionApp.Models
+{
+    public partial class SharedCollection
+    {
+        public int CollectionId { get; set; }
+        public string UserId { get; set; }
+        public bool? ViewRights { get; set; }
+        public bool? EditRights { get; set; }
+
+        public virtual Collection Collection { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
