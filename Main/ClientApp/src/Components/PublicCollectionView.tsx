97328555ad25b7a27c88136728ce269cf2d1a977
arkadR <arkadiusz.rasz@gmail.com>
2020-05-03 12:30:07 +0200

    Updated public collection view

--- Main/ClientApp/src/Components/PublicCollectionView.tsx
+++ Main/ClientApp/src/Components/PublicCollectionView.tsx
@@ -11,19 +11,19 @@ type PublicCollectionViewProps = RouteComponentProps & {
 
 export default function PublicCollectionView(props: PublicCollectionViewProps) {
   let [collection, setCollection] = useState<Collection | null>(null);
+  let [isLoaded, setIsLoaded] = useState(false);
 
-  const getCollection = () => {
-    let returnedCollection = CollectionsApi.getPublicCollection(
-      props.match.params.collectionId
-    );
-    returnedCollection.then((c) => setCollection(c));
-  };
+  CollectionsApi.getPublicCollection(props.match.params.collectionId).then(
+    (c) => {
+      setIsLoaded(true);
+      setCollection(c);
+    }
+  );
 
-  useEffect(() => {
-    getCollection();
-  }, [props.match.params.collectionId]);
-
-  return collection === null ? (
+  //TODO: Refactor, this is unreadable
+  return isLoaded === false ? (
+    <PanelWideMessage withThrobber text="Please wait..." />
+  ) : collection === null ? (
     <PanelWideMessage text="Collection doesn't exists or is private" />
   ) : (
     <BaseCollectionView collection={collection} />
