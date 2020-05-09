b699e97ba15c7e7c116545acfb5557fa4dee7837
arkadR <arkadiusz.rasz@gmail.com>
2020-05-09 18:32:54 +0200

    Added missing migration

---
+++ Main/Migrations/20200509163143_AddedBackPrimaryKey.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LinkCollectionApp.Migrations
+{
+    public partial class AddedBackPrimaryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PublicCollectionVisits",
+                schema: "app",
+                table: "PublicCollectionVisits");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK__PubColVi__37E09F67E467D0D0",
+                schema: "app",
+                table: "PublicCollectionVisits",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK__PubColVi__37E09F67E467D0D0",
+                schema: "app",
+                table: "PublicCollectionVisits");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PublicCollectionVisits",
+                schema: "app",
+                table: "PublicCollectionVisits",
+                column: "Id");
+        }
+    }
+}
