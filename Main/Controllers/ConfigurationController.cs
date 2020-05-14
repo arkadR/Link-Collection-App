5568d145ba3e507f96bb57921fc5337e315bc81a
karol <242557@student.pwr.edu.pl>
2020-05-15 01:22:10 +0200

    ConfigurationController

---
+++ Main/Controllers/ConfigurationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using LinkCollectionApp.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkCollectionApp.Controllers
+{
+  [Authorize(Roles = "Administrator")]
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ConfigurationController : ControllerBase
+  {
+    private readonly ApplicationDbContext _dbContext;
+
+    public ConfigurationController(ApplicationDbContext dbContext)
+    {
+      _dbContext = dbContext;
+    }
+
+    [HttpGet("{key}")]
+    public ActionResult<string> GetValue(string key)
+    {
+      var configuration = _dbContext.Configuration.SingleOrDefault(c => c.Key == key);
+      if (configuration == null)
+        return NotFound();
+      return configuration.Value;
+    }
+  }
+}
