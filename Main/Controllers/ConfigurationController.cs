29da1e5727776beba3ea2570a62dacb9e9c0ad8b
arkadR <arkadiusz.rasz@gmail.com>
2020-05-18 19:14:29 +0200

    Replaced Autorized(Roles) annotation with custom logic

--- Main/Controllers/ConfigurationController.cs
+++ Main/Controllers/ConfigurationController.cs
@@ -6,29 +6,40 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace LinkCollectionApp.Controllers
 {
-  [Authorize(Roles = "Administrator")]
   [Route("api/[controller]")]
   [ApiController]
   public class ConfigurationController : ControllerBase
   {
     private readonly ApplicationDbContext _dbContext;
     private readonly ICollectionConfigurationProvider _configurationProvider;
+    private readonly IUserContextProvider _userProvider;
 
-    public ConfigurationController(ApplicationDbContext dbContext, ICollectionConfigurationProvider configurationProvider)
+    public ConfigurationController(
+      ApplicationDbContext dbContext, 
+      ICollectionConfigurationProvider configurationProvider, 
+      IUserContextProvider userProvider)
     {
       _dbContext = dbContext;
       _configurationProvider = configurationProvider;
+      _userProvider = userProvider;
     }
 
     [HttpGet]
-    public CollectionConfiguration GetConfiguration()
+    public ActionResult<CollectionConfiguration> GetConfiguration()
     {
+      //TODO: Consider extracting to a annotation or an action filter
+      if (_userProvider.IsCurrentUserInRole("Administrator") == false)
+        return Forbid();
+
       return _configurationProvider.GetModel();
     }
 
     [HttpPatch]
     public IActionResult UpdateConfiguration([FromBody] CollectionConfiguration newConfig)
     {
+      if (_userProvider.IsCurrentUserInRole("Administrator") == false)
+        return Forbid();
+
       _configurationProvider.MaxCollectionsPerUser = newConfig.MaxCollectionsPerUser;
       _configurationProvider.MaxElementsInCollection = newConfig.MaxElementsInCollection;
       return Ok();
